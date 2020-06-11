bd1e9f3c6238f6469c0a0dff4d9208f38c19d878
Supakorn Yungpayoon <Supakorn_y@mfec.co.th>
2020-06-11 15:41:51 +0700

    add interface

--- Core101.Reporitory/Base/IRepository.cs
+++ Core101.Reporitory/Base/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -7,8 +8,11 @@ namespace Core101.Reporitory.Base
 {
     public interface IRepository<T> where T : class
     {
+        TResult Query<TResult>(Func<IQueryable<T>, TResult> queryFunction);
         T Get(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] includes);
         IEnumerable<T> GetMany(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] includes);
         T Add(T entity);
+        void Delete(T entity);
+        T Update(T entity);
     }
 }
